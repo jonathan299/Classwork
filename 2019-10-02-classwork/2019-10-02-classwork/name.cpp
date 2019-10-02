9c3a64a7fa713e4ca16d829f5b49b1a2a81eb3f7
jonathan299 <55444026+jonathan299@users.noreply.github.com>
2019-10-02 08:41:37 -0400

    Add files via upload

---
+++ 2019-10-02-classwork/2019-10-02-classwork/name.cpp
@@ -0,0 +1,24 @@
+//Name: Jonathan Vasquez 
+// Intro to CS	 
+// Jonathan Vasquez
+//period 1
+// 2019-10-24- Classwork
+#include <iostream>
+
+int main()
+{
+	char letterC = 'C';
+	char c = 'p';
+	char lmnop = 15;
+	char myName = 'c';
+	short life = 1;
+
+	int periods = 10;
+	unsigned long peterkin = 1000000;
+	// These are bools 
+	bool isHot = true;
+	bool isItFire = false;
+
+	float player = 100;
+	double boss = 1000;
+}
