71fc79def89b97b57fc5306f416098f049666316
jonathan299 <55444026+jonathan299@users.noreply.github.com>
2019-10-03 08:43:03 -0400

    Add files via upload

---
+++ 2019-10-3-Classwork/2019-10-3-Classwork/main.cpp
@@ -0,0 +1,16 @@
+// Name: Jonathan Vasquez
+// Intro to CS
+// Period 1
+// Mr.Willams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "number?  ";
+	int x;
+	cin >> x;
+	cout << "cool, " << x;
+}
