420422b039ed687057704fdbfad6186c33cb738d
jonathan299 <55444026+jonathan299@users.noreply.github.com>
2019-10-04 08:44:33 -0400

    Add files via upload

---
+++ 2019-10-4-classwork/2019-10-4-classwork/Main.cpp
@@ -0,0 +1,33 @@
+//Name: Jonathan Vasquez 
+// Intro to computer science 
+// Mr. Willams 
+// period 1
+// 2019-10-04
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//3
+	float pi = 4.0 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1/ 7.0) + (1 / 9.0) - (1 / 11.0));
+	float pi2 = 4.0 * (1.0 - (1 / 3.0) + (1 / 5.0) - (1 / 7.0) + (1 / 9.0) - (1 / 11.0) + (1 / 13.0));
+	cout << "Pi = " << pi << endl;
+	cout << "Pi2 = " << pi2 << endl;
+
+	//4
+	float c;
+	cout << "Enter a degree in Celsius: 28";
+	cin >> c;
+	float f = (9.0 / 5.0) * c + 32;
+	cout << c << " in fahrenheit is " << f << endl;
+
+	// name: Jonathan Vasquez
+	// Intro to computer science 
+	// Mr. Willams
+	// Period 1
+	// 2019-10-04-Homework
+
+#include <iostream>
+	using namespace std;
+}
